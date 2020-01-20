cb07268a2bfe753db79ceb19aa4377aa21908b72
Ilya Fateev <fat32nov@gmail.com>
2020-01-20 15:58:50 +0300

    initial commit

---
+++ Support/Classes/Core.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Support.Classes
+{
+    public class Core
+    {
+        public void Loop()
+        {
+            while (true)
+            {
+                // pause for second
+                // update all the shit 
+            }
+        }
+
+        public void AddQuery(Query query)
+        {
+            // if there is free operator than give it to him
+            // else add to queue
+        }
+
+
+        public void AddEmployee(Employee employee)
+        {
+            _employees.Add(employee);
+        }
+
+        private Queue<Query> _queries = new Queue<Query>();
+        private List<Employee> _employees = new List<Employee>();
+
+        private uint Tm;
+        private uint Td;
+        private const uint QueryMinTime = 5;
+        private const uint QueryMaxTime = 10;
+    }
+}
---
+++ Support/Classes/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Support.Classes
+{
+    public class Employee
+    {
+        public virtual void Process(Query query)
+        {
+            // get current time
+            _startTime = DateTime.Now;
+            // save it
+
+            // process for some time, get this time?
+        }
+
+        private bool CheckFree()
+        {
+            if (_query == null)
+            {
+                return true;
+            }
+
+            var diff = DateTime.Now - _startTime;
+            if (diff.Seconds > _query.ProcessTimeSec)
+            {
+                _query = null;
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Free => CheckFree();
+        //private bool free;
+
+        private Query _query = null;
+        private DateTime _startTime ;
+    }
+}
